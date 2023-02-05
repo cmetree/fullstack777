61573eabf9af8bf617348cecd520ccb0cba9212a
david shim <99709692+cmetree@users.noreply.github.com>
2023-02-05 22:30:10 +0900

    Update korea-by-age-chart.js

--- coronaboard-web/src/components/chart/korea-by-age-chart.js
+++ coronaboard-web/src/components/chart/korea-by-age-chart.js
@@ -38,6 +38,7 @@ function generateChartOption(data, dataType) {
           const percent = ((obj.value / total) * 100).toFixed(1);
           return `${percent}%`;
         },
+        rotate:-10
       },
       data: ageChartData,
     },
