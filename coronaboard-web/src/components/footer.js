2736e1ba376849f744da8222df9645a8944f3af7
david shim <99709692+cmetree@users.noreply.github.com>
2023-02-05 11:17:09 +0900

    Add files via upload

---
+++ coronaboard-web/src/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import { Container } from 'react-bootstrap';
+
+export function Footer() {
+  return (
+    <div
+      css={css`
+        color: white;
+        background-color: black;
+        text-align: center;
+      `}
+    >
+      <Container
+        css={css`
+          padding: 36px 0;
+        `}
+      >
+        <b># 2023년 다 잘 되실 겁니다.</b>
+      </Container>
+    </div>
+  );
+}
