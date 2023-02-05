95bc046ff5db9645971e11fd7035c6a1bb1c1141
david shim <99709692+cmetree@users.noreply.github.com>
2023-02-05 21:45:16 +0900

    Update korea-chart-slide.js

--- coronaboard-web/src/components/korea-chart-slide.js
+++ coronaboard-web/src/components/korea-chart-slide.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import { Slide } from './slide';
-import { KoreaTestChart } from './chart/korea-test-chart';
 import { KoreaBySexChart } from './chart/korea-by-sex-chart';
 import { KoreaByAgeChart } from './chart/korea-by-age-chart';
 
 export function KoreaChartSlide(props) {
   const { id, dataSource } = props;
   const {
-    koreaTestChartData,
     koreaBySexChartData,
     koreaByAgeChartData,
   } = dataSource;
@@ -22,7 +20,6 @@ export function KoreaChartSlide(props) {
           }
         `}
       >
-        <KoreaTestChart koreaTestChartData={koreaTestChartData} />
         <KoreaBySexChart koreaBySexChartData={koreaBySexChartData} />
         <KoreaByAgeChart koreaByAgeChartData={koreaByAgeChartData} />
       </div>
