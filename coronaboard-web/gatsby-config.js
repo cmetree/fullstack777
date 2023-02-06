78c6d96a03aa7fb65f421d08353927a2edab6b9f
david shim <99709692+cmetree@users.noreply.github.com>
2023-02-07 04:29:12 +0900

    Update gatsby-config.js

--- coronaboard-web/gatsby-config.js
+++ coronaboard-web/gatsby-config.js
@@ -7,8 +7,9 @@ module.exports = {
   ],
   siteMetadata: {
     siteUrl: 'https://davidshim.kr',
-    title: '(COVID-19)코로나 19 한눈에 알아보기',
+    title: '코로나19(COVID-19) 실시간 상황판',
     description:
       '코로나19에 관한 세계 각 국가들의 통계 및 뉴스 등을 취합하여 실시간 정보와 다양한 차트를 제공합니다',
+    image: 'https://davidshim.kr/19.jpg',
   },
 };
