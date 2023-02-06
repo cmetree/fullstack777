80783798a27daf364850c778ad022edf56f1f3b7
david shim <99709692+cmetree@users.noreply.github.com>
2023-02-07 02:27:49 +0900

    Add files via upload

---
+++ coronaboard-web/src/components/helmet-wrapper.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+
+function HelmetWrapper({ title, description }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `,
+  );
+
+  const { siteMetadata } = site;
+  const metaDescription = description || siteMetadata.description;
+
+  return (
+    <Helmet
+      title={title}
+      titleTemplate={`%s | ${siteMetadata.title}`}
+      meta={[
+        {
+          name: 'description',
+          content: metaDescription,
+        },
+
+      ]}
+    />
+  );
+}
+
+
+HelmetWrapper.defaultProps = {
+  description: null,
+};
+
+HelmetWrapper.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+};
+
+export default HelmetWrapper;
