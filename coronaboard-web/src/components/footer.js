178338bd2de7a56d1956ee390f46f68c0e1946f8
david shim <99709692+cmetree@users.noreply.github.com>
2023-02-06 20:50:41 +0900

    Update footer.js

--- coronaboard-web/src/components/footer.js
+++ coronaboard-web/src/components/footer.js
@@ -16,7 +16,7 @@ export function Footer() {
           padding: 36px 0;
         `}
       >
-        <b>❤️ 2023년 다 잘 되실 겁니다. ❤️</b>
+        <b>❤️2023년 다 잘 되실 겁니다.❤️ <br/>Copied by Whocdin(Shim gyu Tae) from coronaboard.kr</b>
       </Container>
     </div>
   ); 
