cca204a2c05d39fa14e39432ce2802ba53156272
david shim <99709692+cmetree@users.noreply.github.com>
2023-02-10 15:22:23 +0900

    Update helmet-wrapper.js

--- coronaboard-web/src/components/helmet-wrapper.js
+++ coronaboard-web/src/components/helmet-wrapper.js
@@ -53,10 +53,7 @@ function HelmetWrapper({ title, description }) {
         },
       ]}
     >
-      <script
-        async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6944089488427159"
-        crossorigin="anonymous"
-      ></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6944089488427159" crossorigin="anonymous"></script>
     </Helmet>
   );
 }
